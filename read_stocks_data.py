3e493b120d0fe1b01246473d6764ca10d2f0c72e
Marcus <marcusdelrio@gmail.com>
2017-08-11 23:37:33 -0400

    First Commit - Read in data for AMD using pandas_datareader package

--- read_stocks_data.py
+++ read_stocks_data.py
@@ -1,1 +1,9 @@
 #read in stocks data via googlefinance package
+
+%pylab
+import pandas_datareader.data as web
+import datetime
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2013, 1, 27)
+amd = web.DataReader("AMD", 'google', start, end)
+amd.Close.plot()
